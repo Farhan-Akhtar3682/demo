73f8e7637b1a15cd11aa1c46bd0069caa68d76c9
Farhan-Akhtar3682 <m.farhanakhtar04@gmail.com>
2026-07-29 14:37:39 +0500

    add the Dockerfile

--- demo.js
+++ demo.js
@@ -2,3 +2,6 @@ var message = "Hello, World!";
 var description = "This is a demo JavaScript file.";
 console.log(message);
 console.log(description);
+console.log("my name is Farhan")
+var a="hyghu"
+console.log("program is ended")
